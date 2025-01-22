26c34eb5442c7326a663f6cc6c68205242ce6f14
Anny Monteiro <anerebecaluz@gmail.com>
2025-01-22 11:40:46 -0300

    Domain model

---
+++ src/main/java/com/annyprojetos/dslist/entities/BelongingPK.java
@@ -0,0 +1,55 @@
+package com.annyprojetos.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable //4
+public class BelongingPK {
+    @ManyToOne //1
+    @JoinColumn(name = "game_id") //2
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "gameList_id")
+    private GameList gameList;
+
+    public BelongingPK(){
+    }
+
+    public BelongingPK(Game game, GameList gameList) {
+        this.game = game;
+        this.gameList = gameList;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public GameList getGameList() {
+        return gameList;
+    }
+
+    public void setGameList(GameList gameList) {
+        this.gameList = gameList;
+    }
+
+    //3
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        BelongingPK that = (BelongingPK) o;
+        return Objects.equals(game, that.game) && Objects.equals(gameList, that.gameList);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, gameList);
+    }
+}
